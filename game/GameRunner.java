1d8ad3d1c551fd4f0bc9112d80f5297e1f445729
stroganova <dasses@yandex.ru>
2023-10-10 00:22:02 +0300

    Add files via upload

---
+++ game/GameRunner.java
@@ -0,0 +1,11 @@
+package com.github.stroganova.game;
+
+public class GameRunner {
+public static void main (String[] args) {
+    Player player = new Player("Arthur", 100, 10, 15, 1, 6);
+    Monster monster = new Monster(20, 17, 3, 3, 9);
+    player.attack(monster);
+    monster.attack(player);
+    player.selfHeal();
+}
+}
